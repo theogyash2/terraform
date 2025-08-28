e9b3ecc8423833b475b6edaf2c6d03017c044d1b
Yash <Yash@MacBook-Pro.local>
2025-08-28 15:05:49 +0530

    terraformfile

---
+++ terraform.tf
@@ -0,0 +1,32 @@
+terraform {
+
+
+  # backend "remote" {
+  #   hostname     = "app.terraform.io"
+  #   organization = "new-cloud-terraform"
+
+  #   workspaces {
+  #     name = "my-aws-app"
+  #   }
+  # }
+
+  required_providers {
+
+    aws = {
+      source     = "hashicorp/aws"
+      version    = ">=6.9.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
+  }
+}
